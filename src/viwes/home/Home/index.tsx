832b527db21889169d2e6633ab4c93d021985c15
BIGREDFLOWER <1350983229@qq.com>
2025-05-06 05:11:53 +0800

    修正路由

--- src/viwes/home/Home/index.tsx
+++ src/viwes/home/Home/index.tsx
@@ -1,19 +1,20 @@
-import { Outlet } from "react-router-dom";
+import {Outlet} from "react-router-dom";
 import Carousel from "../Carousel";
 import Nav from "../nav";
 import Rplaylists from "../Rplaylists";
 import Foot from "../../Foot";
 import Bfq from "../../Bfq";
-const Home =()=>{
-return (
-    <div>
-        <Nav/>
-    <Carousel/>
-    <Rplaylists />
-    <Outlet/>
-    <Foot />
-    <Bfq/>
-    </div>
-)
+
+const Home = () => {
+    return (
+        <div>
+            <Nav/>
+            <Carousel/>
+            <Rplaylists/>
+            <Outlet/>
+            <Foot/>
+            <Bfq/>
+        </div>
+    )
 }
 export default Home;
